1881b81479dfb76084ab5fa7e95293cdbdf0c113
joearpy <joearpy87@gmail.com>
2020-12-15 21:20:18 +0100

    127 - Rendering Project Lists

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -39,6 +39,33 @@ function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
     return adjDescriptor;
 }
 
+class ProjectList {
+    element: HTMLElement;
+    hostElement: HTMLDivElement;
+    templateElement: HTMLTemplateElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 class ProjectInput {
     descriptionInputElement: HTMLInputElement;
     element: HTMLFormElement;
@@ -122,3 +149,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
